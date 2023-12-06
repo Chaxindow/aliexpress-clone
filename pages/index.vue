bd17d913dfdfb4bef5acd18aa417e780eafbc103
Chaxindow <78871407+Chaxindow@users.noreply.github.com>
2023-12-06 12:51:52 +0300

    layouts yerine layout yazıldı

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-    <MainLayouts>
+    <MainLayout>
         <div id = "IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
         </div>
-    </MainLayouts>
+    </MainLayout>
 </template>
 
 <script setup>
-import MainLayouts from '../layouts/MainLayouts.vue';
+import MainLayout from '../layouts/MainLayout.vue';
 
 
 </script>
