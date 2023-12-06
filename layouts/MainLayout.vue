0cae09cbf78e0cc263a7214cede20619c1921c3c
Chaxindow <78871407+Chaxindow@users.noreply.github.com>
2023-12-06 14:28:14 +0300

    branch deneme 2

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="MainLayout" class="w-full fixed z-50">
         <div id="TopMenu" class="w-full bg-[#FAFAFAF] border-b md:block hidden">
-            
+            Mete deneme
         </div>
     </div>
 </template>
