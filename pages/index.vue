e3207db589e24984346d94eb18f510f71de728db
Chaxindow <78871407+Chaxindow@users.noreply.github.com>
2023-12-07 15:51:44 +0300

    Layout sayfası bitti Laoding vue eklendi

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
     <MainLayout>
-        <div id = "IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+        <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+            <div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+
+            </div>
         </div>
     </MainLayout>
 </template>
@@ -11,6 +14,4 @@ import MainLayout from '~/layouts/MainLayout.vue';
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
