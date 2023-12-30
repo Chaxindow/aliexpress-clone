97f425c4801e0ed278e358d759e48a2c9d8fd9d9
Chaxindow <78871407+Chaxindow@users.noreply.github.com>
2023-12-30 18:19:20 +0300

    Adress page CheckoutItem TextInput Checkout yapıldı

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="MainLayout" class="w-full fixed z-50">
+  <div id="MainLayout" class="w-full fixed z-50 ">
     <div id="TopMenu" class="w-full bg-[#FAFAFAF] border-b md:block hidden">
 
       <ul class="flex
